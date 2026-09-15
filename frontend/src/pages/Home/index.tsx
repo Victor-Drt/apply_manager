40100b24d4094c43f45b897fc4e1b5f570fd0333
Victor-Drt <victorduarte778@gmail.com>
2026-09-15 11:15:11 -0400

    feat: adiciona estilo que sera seguido na home

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+
+
+const HomePage = () => {
+
+    return (
+
+        <main className="content">
+            <h1>Home</h1>
+        </main>
+    )
+}
+
+export default HomePage;
