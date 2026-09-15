27b57997ded99436c5646a53ec61f421003f0837
Victor-Drt <victorduarte778@gmail.com>
2026-09-14 22:14:56 -0400

    feat: adiciona endpoint de health check

---
+++ backend/app/api/router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from app.api.endpoints import applications, auth, health, users
+
+api_router = APIRouter()
+
+
+api_router.include_router(health.router, prefix="/health", tags=["health"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(
+    applications.router, prefix="/applications", tags=["applications"]
+)
